b4387e5c04bdc1db3aaf93e135cfd7db625c7442
Daniel <bukaro.dany@gmail.com>
2020-08-13 23:27:45 -0500

    Accoutns App
    Creating the accoutns app to Creathe a customer user model

--- club_app/settings.py
+++ club_app/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #Local
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
